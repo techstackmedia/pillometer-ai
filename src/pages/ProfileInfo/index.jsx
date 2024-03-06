50aebaaeda9ca6368eaa7f3f42eb87d067998e70
Osagie Bello <info@techstackmedia.com>
2024-03-06 16:28:03 +0100

    build: create profile info component or ui

---
+++ src/pages/ProfileInfo/index.jsx
@@ -0,0 +1,19 @@
+import Alert from '../../components/Alert';
+import Navbar from '../../components/Navbar';
+import Profile from '../../components/Profile';
+import UsageInfoInput from '../../components/UsageInfoInput';
+
+const ProfileInfo = () => {
+  return (
+    <>
+      <Navbar />
+      <div className='App' style={{ alignItems: 'flex-start' }}>
+        <Profile />
+        <Alert />
+        <UsageInfoInput />
+      </div>
+    </>
+  );
+};
+
+export default ProfileInfo;
