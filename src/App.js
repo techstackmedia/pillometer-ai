3e339b648f528274c88eec1fbddddc417ba76d85
Osagie Bello <info@techstackmedia.com>
2024-03-06 16:26:22 +0100

    feat: provide route to /auth/profile page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Auth from './pages/Auth';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ProfileInfo from './pages/ProfileInfo';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className='App'>
+      <div>
         <Routes>
           <Route path='/auth' element={<Auth />} />
+          <Route path='/auth/profile' element={<ProfileInfo />} />
         </Routes>
       </div>
     </BrowserRouter>
