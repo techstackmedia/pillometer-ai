ab014daeb52ca593062e7125525cd3c255d2c15b
Osagie Bello <info@techstackmedia.com>
2024-03-06 16:15:54 +0100

    style: change first and last name input width

--- src/components/Authentication/Register/index.jsx
+++ src/components/Authentication/Register/index.jsx
@@ -26,7 +26,7 @@ const Register = ({
           value={firstName}
           onChange={handleFirstNameChange}
           placeholder='First Name'
-          sx={{ width: 230 }}
+          sx={{ width: 280 }}
         />
         <Input
           type='text'
@@ -34,7 +34,7 @@ const Register = ({
           value={lastName}
           onChange={handleLastNameChange}
           placeholder='Last Name'
-          sx={{ width: 230 }}
+          sx={{ width: 280 }}
         />
       </div>
       <div className={styles.dropdownInput}>
