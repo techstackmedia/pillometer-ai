544362d307e5d177a63951135404fae07dda91ac
Osagie Bello <osagienoah04@gmail.com>
2024-03-06 09:57:24 +0100

    style(css): animate input label also by its opacity within 0.3s delay

--- src/components/shared/Input/index.jsx
+++ src/components/shared/Input/index.jsx
@@ -13,11 +13,10 @@ const Input = ({ type, placeholder, name, value, onChange, sx, cn }) => {
         onChange={onChange}
         style={sx}
       />
-      {value?.length > 0 && (
-        <label htmlFor={name} className={styles['input-label']}>
-          {placeholder}
-        </label>
-      )}
+
+      <label htmlFor={name} className={styles['input-label']}>
+        {placeholder}
+      </label>
     </div>
   );
 };
