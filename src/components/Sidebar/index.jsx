5ff19c7200e1fb478d299a27e9dc26ccb759d395
Osagie Bello <info@techstackmedia.com>
2024-03-07 10:39:52 +0100

    build(ui): create fixed sidebar component

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,47 @@
+import AddIcon from '../../images/add.png';
+import Content from '../shared/Content';
+import styles from './index.module.css';
+import editPenIcon from '../../images/editPen.png';
+const Sidebar = () => {
+  return (
+    <div className={styles.sidebar}>
+      <div className={styles.main}>
+        <div className={styles.section}>
+          <button className={styles.button}>
+            <span>New Chat</span> <img src={AddIcon} alt='add icon' />
+          </button>
+        </div>
+        <div className={styles.section}>
+          <Content cn={styles.paragraph}>Libraries</Content>
+          <Content cn={styles.tab}>Symptoms</Content>
+          <Content cn={styles.tab}>Conditions</Content>
+        </div>
+        <div className={styles.section}>
+          <Content cn={styles.paragraph}>near me</Content>
+          <Content cn={styles.tab}>Pharmacies</Content>
+          <Content cn={styles.tab}>Hospitals</Content>
+        </div>
+        <div className={styles.section}>
+          <Content cn={styles.tab}>Community</Content>
+          <Content cn={styles.tab}>Help & Feedback</Content>
+        </div>
+      </div>
+
+      <div className={styles.main}>
+        <div className={styles.section}>
+          <Content cn={styles.paragraph}>Your Location:</Content>
+          <div className={styles.address}>
+            <Content cn={styles.tab}>
+              29 Taiwo St, Ojota, Lagos 105102, Lagos
+            </Content>
+            <button>
+              <img src={editPenIcon} alt='edit pen icon' />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
