b190806ca8403ab13eec16a4bdfa313e34d337f0
Osagie Bello <info@techstackmedia.com>
2024-03-08 03:45:10 +0100

    style: change the max-height of Detail class

--- src/components/ChatResponseList/index.jsx
+++ src/components/ChatResponseList/index.jsx
@@ -1,11 +1,16 @@
+import { useLocation } from 'react-router-dom';
 import Chat from '../Chat';
 import ChatResponse from '../ChatResponse';
 import styles from './index.module.css';
 
 const ChatResponseList = () => {
+  const { pathname } = useLocation();
   return (
     <main className={styles.main}>
-      <div className='Detail'>
+      <div
+        className='Detail'
+        style={{ maxHeight: pathname === '/community' ? 'auto' : '75vh' }}
+      >
         <ChatResponse />
         <ChatResponse />
         <ChatResponse />
