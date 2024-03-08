0a8a79ea771b52f88aab570d5fdc5e478997e9ab
Osagie Bello <info@techstackmedia.com>
2024-03-08 02:23:15 +0100

    feat: use testing route, /detail to render the chat qa detail component or page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ProfileInfo from './pages/ProfileInfo';
 import Home from './pages/Home';
 import Community from './pages/Community';
+import ChatQADetail from './pages/ChatQADetail';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path='/auth' element={<Auth />} />
           <Route path='/auth/profile' element={<ProfileInfo />} />
           <Route path='/community' element={<Community />} />
+          <Route path='/detail' element={<ChatQADetail />} />
         </Routes>
       </div>
     </BrowserRouter>
