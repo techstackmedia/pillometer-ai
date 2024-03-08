9b0dd108d343263eb81160592ff71858b76a0970
Osagie Bello <info@techstackmedia.com>
2024-03-08 03:33:36 +0100

    build: create question list component contain the list of question components

---
+++ src/components/QuestionList/index.jsx
@@ -0,0 +1,16 @@
+import Question from '../Question';
+
+const QuestionList = () => {
+  return (
+    <div>
+      <Question />
+      <Question />
+      <Question />
+      <Question />
+      <Question />
+      <Question />
+    </div>
+  );
+};
+
+export default QuestionList;
