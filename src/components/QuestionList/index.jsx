68aa5c62deb534db737abd4c8adc6a158f6f990f
Osagie Bello <info@techstackmedia.com>
2024-03-09 06:58:03 +0100

    chore: change to the use of react fragment instead of div

--- src/components/QuestionList/index.jsx
+++ src/components/QuestionList/index.jsx
@@ -2,14 +2,14 @@ import Question from '../Question';
 
 const QuestionList = () => {
   return (
-    <div>
+    <>
       <Question />
       <Question />
       <Question />
       <Question />
       <Question />
       <Question />
-    </div>
+    </>
   );
 };
 
