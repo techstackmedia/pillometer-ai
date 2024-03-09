26d43c0dbc86a49586a9410ccba2c8f23966715f
Osagie Bello <info@techstackmedia.com>
2024-03-09 07:44:50 +0100

    refactor: refactor forum component to display banner, left input search right filter button, left and right card qa components

--- src/components/Forum/index.jsx
+++ src/components/Forum/index.jsx
@@ -1,26 +1,18 @@
 import Banner from '../shared/Banner';
 import Content from '../shared/Content';
-import Input from '../shared/Input';
 import styles from './index.module.css';
-import searchIcon from '../../images/searchCommunity.png';
-import Button from '../shared/Button';
-import Card from '../shared/Card';
-import dropdownIcon from '../../images/arrowDropDown.png';
-import thumbUpIcon from '../../images/thumbUp.png';
-import barChatIcon from '../../images/barChart.png';
-import Tags from '../Tag';
-import QuestionList from '../QuestionList';
-import ChatResponseList from '../ChatResponseList';
+import LeftCardQA from './LeftCardQA';
+import RightCardQA from './RightCardQA';
+import RightFilterButton from './RightFilterButton';
+import LeftInputSearch from './LeftInputSearch';
 
 const Forum = () => {
   return (
     <div className={styles.main}>
       <Banner cn={styles.banner}>
-        <div>
-          <Content cn={`heading ${styles.heading}`}>
-            Your Ultimate Wellness Partner
-          </Content>
-        </div>
+        <Content cn={`heading ${styles.heading}`}>
+          Your Ultimate Wellness Partner
+        </Content>
         <Content cn={`paragraph ${styles.paragraph}`}>
           Your medical data and health information is confidential, protected
           and secured with end-to-end encryption.
@@ -30,50 +22,13 @@ const Forum = () => {
         </Content>
       </Banner>
       <div className={styles.topics}>
-        <form className={styles.searchForm}>
-          <Input type='search' placeholder='Search topics of interest' />
-          <img className={styles.img} src={searchIcon} alt='search icon' />
-        </form>
-        <form className={styles.filterForm}>
-          <Button>
-            Most recent
-            <img src={dropdownIcon} alt='dropdown icon' />
-          </Button>
-          <Button>
-            Informational
-            <img src={dropdownIcon} alt='dropdown icon' />
-          </Button>
-          <Button>
-            Prescriptions
-            <img src={dropdownIcon} alt='dropdown icon' />
-          </Button>
-        </form>
+        <LeftInputSearch />
+        <RightFilterButton />
       </div>
 
       <div className={styles.topics}>
-        <div className={`${styles['scroll-hidden']} ${styles.questions}`}>
-          <QuestionList />
-        </div>
-        <form className={styles.cardForm}>
-          <Card cn={styles.qa}>
-            <div className={styles.viewLikes}>
-              <div className={styles.postView}>Posted 3 days ago</div>
-              <div className={styles.likesSection}>
-                <div className={styles.likes}>
-                  <img src={barChatIcon} alt='thumbs up icon' />
-                  <span>514</span>
-                </div>
-                <div className={styles.likes}>
-                  <span>304</span>
-                  <img src={thumbUpIcon} alt='thumb up icon' />
-                </div>
-              </div>
-            </div>
-            <Tags cn={styles.tags} />
-            <hr />
-            <ChatResponseList />
-          </Card>
-        </form>
+        <LeftCardQA />
+        <RightCardQA />
       </div>
     </div>
   );
