667f43354fd4b1da1b81125fccb7ad794608134f
Osagie Bello <info@techstackmedia.com>
2024-03-12 00:28:04 +0100

    refactor: conditionally render login and register form. remove profile component from file

--- src/components/Authentication/index.jsx
+++ src/components/Authentication/index.jsx
@@ -1,16 +1,13 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import Button from '../shared/Button';
-import Content from '../shared/Content';
-import Logo from '../../logo.svg';
 import Login from './Login';
 import Register from './Register';
-import Profile from './Profile';
 import { useLocation } from 'react-router-dom';
 import styles from './index.module.css';
 import { AuthSigninContext } from '../../context/Auth/Signin';
 import { AuthSignupContext } from '../../context/Auth/Register';
 
-const Authentication = ({ isCurrentPage /* , navigateToNextPage */ }) => {
+const Authentication = () => {
   const {
     email,
     password,
@@ -43,69 +40,16 @@ const Authentication = ({ isCurrentPage /* , navigateToNextPage */ }) => {
   } = useContext(AuthSignupContext);
 
   const { pathname } = useLocation();
-  const [keepUpWithCommunity, setKeepUpWithCommunity] = useState(false);
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [profession, setProfession] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [discover, setDiscover] = useState('');
-  const handleFirstNameChange = (e) => {
-    setFirstName(e.target.value);
-  };
-
-  const handleLastNameChange = (e) => {
-    setLastName(e.target.value);
-  };
-
-  const handlePhoneNumberChange = (e) => {
-    setPhoneNumber(e.target.value);
-  };
-
-  const handleProfessionChange = (e) => {
-    setProfession(e.target.value);
-  };
-
-  const handleDiscoverChange = (e) => {
-    setDiscover(e.target.value);
-  };
-
-  const handleCommunityCheckboxChange = (e) => {
-    setKeepUpWithCommunity(e.target.checked);
-  };
 
   return (
     <>
-      <img src={Logo} alt='logo' className='logo' />
-      <Content cn='heading'>
-        {isCurrentPage ? 'Help us know you better' : "Let's Get You Started"}
-      </Content>
-      <Content cn={`paragraph ${styles.info}`}>
-        {isCurrentPage
-          ? 'This information will help us curate a personalized experience'
-          : null}
-      </Content>
-      {pathname !== '/auth/register' ? (
+      {pathname === '/auth/login' ? (
         <form
           className={styles.form}
-          onSubmit={pathname === '/auth/profile' ? null : handleSigninSubmit}
+          onSubmit={handleSigninSubmit}
           autoComplete='off'
         >
-          {isCurrentPage ? (
-            <Profile
-              discover={discover}
-              lastName={lastName}
-              firstName={firstName}
-              profession={profession}
-              phoneNumber={phoneNumber}
-              keepUpWithCommunity={keepUpWithCommunity}
-              handleDiscoverChange={handleDiscoverChange}
-              handleLastNameChange={handleLastNameChange}
-              handleFirstNameChange={handleFirstNameChange}
-              handleProfessionChange={handleProfessionChange}
-              handlePhoneNumberChange={handlePhoneNumberChange}
-              handleCommunityCheckboxChange={handleCommunityCheckboxChange}
-            />
-          ) : (
+          {
             <Login
               email={email}
               password={password}
@@ -121,14 +65,8 @@ const Authentication = ({ isCurrentPage /* , navigateToNextPage */ }) => {
               showPassword={showPassword}
               successMessage={successMessage}
             />
-          )}
-          <Button
-            type='submit'
-            // navigateToNextPage={handleAuthNavigation}
-            isCurrentPage={isCurrentPage}
-          >
-            {pathname === '/auth/login' ? 'Sign in' : 'Proceed'}
-          </Button>
+          }
+          <Button type='submit'>Sign in</Button>
         </form>
       ) : (
         <form
@@ -152,13 +90,7 @@ const Authentication = ({ isCurrentPage /* , navigateToNextPage */ }) => {
             successMessageReg={successMessageReg}
           />
 
-          <Button
-            type='submit'
-            // navigateToNextPage={navigateToNextPage}
-            isCurrentPage={isCurrentPage}
-          >
-            Create Account
-          </Button>
+          <Button type='submit'>Create Account</Button>
         </form>
       )}
     </>
