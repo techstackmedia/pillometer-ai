9869cf5359d7492e758a2a3eb20e586f41ce2741
Osagie Bello <info@techstackmedia.com>
2024-03-12 00:36:29 +0100

    refactor: refactor code

--- src/components/UsageInfoInput/index.jsx
+++ src/components/UsageInfoInput/index.jsx
@@ -20,68 +20,66 @@ const UsageInfoInput = () => {
         For a better experience, kindly tell us who or why you're using
         pillometer.ai
       </Content>
-      <form>
-        <div className={styles.cardContainer}>
-          <Card cn={styles.card}>
-            <div className={styles.iconRadio}>
-              <img src={personIcon} alt='person icon' />
-              <input
-                type='radio'
-                checked={selectedOption === 'personal'}
-                id='personal'
-                onChange={() => handleOptionChange('personal')}
-              />
-            </div>
-            <Content cn={`heading ${styles.usageInfoHeading}`}>
-              Personal
-            </Content>
-            <Content cn='paragraph'>
-              Welcome to HealthBot; Your Personal Health Companion on the
-              Journey to Wellness
-            </Content>
-          </Card>
+      <div className={styles.cardContainer}>
+        <Card cn={styles.card}>
+          <div className={styles.iconRadio}>
+            <img src={personIcon} alt='person icon' />
+            <input
+              type='radio'
+              checked={selectedOption === 'personal'}
+              id='personal'
+              onChange={() => handleOptionChange('personal')}
+            />
+          </div>
+          <Content cn={`heading ${styles.usageInfoHeading}`}>Personal</Content>
+          <Content cn='paragraph'>
+            Welcome to HealthBot; Your Personal Health Companion on the Journey
+            to Wellness
+          </Content>
+        </Card>
 
-          <Card cn={styles.card}>
-            <div className={styles.iconRadio}>
-              <img src={personsGroupIcon} alt='person icon' />
-              <input
-                type='radio'
-                checked={selectedOption === 'friends-family'}
-                name='usageInfo'
-                id='friends-family'
-                onChange={() => handleOptionChange('friends-family')}
-              />
-            </div>
-            <Content cn={`heading ${styles.usageInfoHeading}`}>
-              Friends/Family
-            </Content>
-            <Content cn='paragraph'>
-              Welcome to HealthBot; Your Personal Health Companion on the
-              Journey to Wellness
-            </Content>
-          </Card>
+        <Card cn={styles.card}>
+          <div className={styles.iconRadio}>
+            <img src={personsGroupIcon} alt='person icon' />
+            <input
+              type='radio'
+              checked={selectedOption === 'friends-family'}
+              name='usageInfo'
+              id='friends-family'
+              onChange={() => handleOptionChange('friends-family')}
+            />
+          </div>
+          <Content cn={`heading ${styles.usageInfoHeading}`}>
+            Friends/Family
+          </Content>
+          <Content cn='paragraph'>
+            Welcome to HealthBot; Your Personal Health Companion on the Journey
+            to Wellness
+          </Content>
+        </Card>
 
-          <Card cn={`${styles.card} ${styles.cardMg}`}>
-            <div className={styles.iconRadio}>
-              <img src={manageSearch} alt='person icon' />
-              <input
-                type='radio'
-                checked={selectedOption === 'health-consultant'}
-                id='health-consultant'
-                onChange={() => handleOptionChange('health-consultant')}
-              />
-            </div>
-            <Content cn={`heading ${styles.usageInfoHeading}`}>
-              Health Consultant
-            </Content>
-            <Content cn='paragraph'>
-              Welcome to HealthBot; Your Personal Health Companion on the
-              Journey to Wellness
-            </Content>
-          </Card>
-        </div>
-        <Button cn={styles.proceedButton}>Proceed</Button>
-      </form>
+        <Card cn={`${styles.card} ${styles.cardMg}`}>
+          <div className={styles.iconRadio}>
+            <img src={manageSearch} alt='person icon' />
+            <input
+              type='radio'
+              checked={selectedOption === 'health-consultant'}
+              id='health-consultant'
+              onChange={() => handleOptionChange('health-consultant')}
+            />
+          </div>
+          <Content cn={`heading ${styles.usageInfoHeading}`}>
+            Health Consultant
+          </Content>
+          <Content cn='paragraph'>
+            Welcome to HealthBot; Your Personal Health Companion on the Journey
+            to Wellness
+          </Content>
+        </Card>
+      </div>
+      <Button type='submit' cn={styles.proceedButton}>
+        Proceed
+      </Button>
     </>
   );
 };
