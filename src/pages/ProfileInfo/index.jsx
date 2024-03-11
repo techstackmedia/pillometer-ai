fa77880a944f77145cc34214e06f2d6595fe71d2
Osagie Bello <info@techstackmedia.com>
2024-03-12 00:45:03 +0100

    feat: import profile input text component and render or return it in app

--- src/pages/ProfileInfo/index.jsx
+++ src/pages/ProfileInfo/index.jsx
@@ -1,23 +1,11 @@
-import Alert from '../../components/shared/Alert';
-import Navbar from '../../components/Navbar';
-import Profile from '../../components/Profile';
-import UsageInfoInput from '../../components/UsageInfoInput';
+import ProfileInputText from '../../components/Authentication/ProfileInputText';
 import styles from './index.module.css';
 
 const ProfileInfo = () => {
   return (
-    <>
-      <Navbar />
-      <div className={`App ${styles.App}`}>
-        <Profile />
-        <Alert>
-          Your medical data and health information is confidential, protected
-          and secured with end-to-end encryption. We are in compliance with ISO
-          9145930 data protection law.
-        </Alert>
-        <UsageInfoInput />
-      </div>
-    </>
+    <div className={styles.App}>
+      <ProfileInputText />
+    </div>
   );
 };
 
