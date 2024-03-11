bbb294eed4459cf194be267f659a9e6d7da4072e
Osagie Bello <info@techstackmedia.com>
2024-03-11 17:24:02 +0100

    feat: provide logic to show alert messages when an error occur during login or registration process

--- src/pages/Auth/index.jsx
+++ src/pages/Auth/index.jsx
@@ -1,23 +1,46 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import AuthLiner from '../../components/AuthLiner';
 import Authentication from '../../components/Authentication';
 import OAuthorization from '../../components/OAuthorization';
 import '../../App.css';
+import { useLocation } from 'react-router-dom';
+import { AuthSigninContext } from '../../context/Auth/Signin';
+import { AuthSignupContext } from '../../context/Auth/Register';
+import Alert from '../../components/shared/Alert';
+import styles from './index.module.css';
+import Content from '../../components/shared/Content';
 
 const Auth = () => {
   const [isCurrentPage, setIsCurrentPage] = useState(false);
   const navigateToNextPage = () => {
     setIsCurrentPage(true);
   };
+  const { pathname } = useLocation();
+
+  const { signinError } = useContext(AuthSigninContext);
+  const { signupError } = useContext(AuthSignupContext);
 
   return (
     <div className='App'>
+      <>
+        {(signinError || signupError) && (
+          <div className={styles.alert}>
+            <Alert>
+              <Content>{signinError || signupError}</Content>
+            </Alert>
+          </div>
+        )}
+      </>
       <Authentication
         navigateToNextPage={navigateToNextPage}
         isCurrentPage={isCurrentPage}
       />
-      <AuthLiner isCurrentPage={isCurrentPage} />
-      <OAuthorization isCurrentPage={isCurrentPage} />
+      {pathname === '/auth/register' && (
+        <>
+          <AuthLiner isCurrentPage={isCurrentPage} />
+          <OAuthorization isCurrentPage={isCurrentPage} />
+        </>
+      )}
     </div>
   );
 };
