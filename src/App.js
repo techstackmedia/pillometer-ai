a880b6ffa13d14e91cc4b31b32d79ecc09164dd1
Osagie Bello <info@techstackmedia.com>
2024-03-11 17:30:33 +0100

    feat: login and register pages or routes sharing same auth component and provision of routing for email verification page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import './App.css';
-import Auth from './pages/Auth';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ProfileInfo from './pages/ProfileInfo';
 import Home from './pages/Home';
 import Community from './pages/Community';
 import ChatQADetail from './pages/ChatQADetail';
+import Auth from './pages/Auth';
+import { AuthSigninProvider } from './context/Auth/Signin';
+import EmailVerification from './pages/EmailVerification';
+import { AuthSignupProvider } from './context/Auth/Register';
 
 function App() {
   return (
     <BrowserRouter>
-      <>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path='/auth' element={<Auth />} />
-          <Route path='/auth/profile' element={<ProfileInfo />} />
-          <Route path='/community' element={<Community />} />
-          <Route path='/detail' element={<ChatQADetail />} />
-        </Routes>
-      </>
+      <AuthSignupProvider>
+        <AuthSigninProvider>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path='/auth/login' element={<Auth />} />
+            <Route path='/auth/register' element={<Auth />} />
+            <Route
+              path='/auth/email-verification'
+              element={<EmailVerification />}
+            />
+            <Route path='/auth/profile' element={<ProfileInfo />} />
+            <Route path='/community' element={<Community />} />
+            <Route path='/detail' element={<ChatQADetail />} />
+          </Routes>
+        </AuthSigninProvider>
+      </AuthSignupProvider>
     </BrowserRouter>
   );
 }
