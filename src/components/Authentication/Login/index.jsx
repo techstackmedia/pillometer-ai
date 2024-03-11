a1f2195b55717be3e7271b65b8103d84ebd120ec
Osagie Bello <info@techstackmedia.com>
2024-03-11 17:00:04 +0100

    feat(style): show alert message when error occurs or when registration was successful and style it

--- src/components/Authentication/Login/index.jsx
+++ src/components/Authentication/Login/index.jsx
@@ -1,22 +1,63 @@
 import Input from '../../shared/Input';
-import Agreement from '../../Agreement';
 import styles from './index.module.css';
+import { useContext } from 'react';
+import { AuthSigninContext } from '../../../context/Auth/Signin';
+import { useLocation } from 'react-router-dom';
+import Alert from '../../shared/Alert';
+import Content from '../../shared/Content';
+import { AuthSignupContext } from '../../../context/Auth/Register';
 
-const Login = ({
-  email,
-  password,
-  showPassword,
-  keepSignedIn,
-  handleEmailChange,
-  handlePasswordChange,
-  handleLoginCheckboxChange,
-  togglePasswordVisibility,
-}) => {
+const Login = () => {
+  const {
+    email,
+    password,
+    keepSignedIn,
+    signinError,
+    // isSigningIn,
+    handleEmailChange,
+    handlePasswordChange,
+    handleLoginCheckboxChange,
+    togglePasswordVisibility,
+    showPassword,
+  } = useContext(AuthSigninContext);
+  const { successMessageReg } = useContext(AuthSignupContext);
+  const location = useLocation();
+  const verifiedEmail = location.state?.email;
+
   return (
     <>
+      {verifiedEmail && successMessageReg && (
+        <div className={styles.alert}>
+          <Alert>
+            <Content cn={`heading ${styles.heading}`}>
+              {successMessageReg}
+            </Content>
+            <Content cn={`paragraph ${styles.paragraphText}`}>
+              Login to continue
+            </Content>
+          </Alert>
+        </div>
+      )}
+      <style>{`
+        input[type='email'] {
+          border: ${
+            String(signinError).toLowerCase().split(' ').includes('email')
+              ? '1px solid red'
+              : ''
+          };
+        }
+
+        input[type='password'] {
+          border: ${
+            String(signinError).toLowerCase().split(' ').includes('password')
+              ? '1px solid red'
+              : ''
+          };
+        }
+      `}</style>
       <Input
         type='email'
-        placeholder='Email address'
+        placeholder={verifiedEmail ? 'Confirm email' : 'Email address'}
         name='email'
         value={email}
         onChange={handleEmailChange}
@@ -24,7 +65,7 @@ const Login = ({
       <div className={styles.inputPassword}>
         <Input
           type={showPassword ? 'text' : 'password'}
-          placeholder='Create password'
+          placeholder={verifiedEmail ? 'Confirm password' : 'Create password'}
           name='password'
           value={password}
           onChange={handlePasswordChange}
@@ -44,16 +85,6 @@ const Login = ({
         />
         <span>Keep me signed in</span>
       </label>
-      <Agreement>
-        By continuing, you acknowledge you have read and agreed to our{' '}
-        <a href='/#' target='_blank' rel='noopener noreferrer'>
-          Teams of Use
-        </a>{' '}
-        and{' '}
-        <a href='/#' target='_blank' rel='noopener noreferrer'>
-          Privacy Policy
-        </a>
-      </Agreement>
     </>
   );
 };
