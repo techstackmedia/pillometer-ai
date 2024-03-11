06e15001fd2fa53ac008929d7c7ffe04c0d8501b
Osagie Bello <info@techstackmedia.com>
2024-03-11 17:27:05 +0100

    feat(css): provide styled alert component to check when sign in process is successful

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,26 @@
 import Sidebar from '../../components/Sidebar';
 import Navbar from '../../components/Navbar';
 import Main from '../../components/Main';
+import { useLocation } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthSigninContext } from '../../context/Auth/Signin';
+import Alert from '../../components/shared/Alert';
+import styles from './index.module.css';
 
 const Home = () => {
+  const location = useLocation();
+  const token = location.state.token;
+  const status = location.state.status;
+  const { successMessage } = useContext(AuthSigninContext);
+  localStorage.setItem('token', token);
+
   return (
     <>
+      {status && successMessage && (
+        <div className={styles.homeAlert}>
+          <Alert>Login Successful</Alert>
+        </div>
+      )}
       <Navbar />
       <div className='Main'>
         <Sidebar />
