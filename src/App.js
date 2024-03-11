65da5ed37e351f8786c947f81ffd505424cb3bf3
Osagie Bello <info@techstackmedia.com>
2024-03-12 00:50:16 +0100

    feat: import profile file or module instead of profile info module

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import ProfileInfo from './pages/ProfileInfo';
 import Home from './pages/Home';
 import Community from './pages/Community';
 import ChatQADetail from './pages/ChatQADetail';
@@ -8,24 +7,28 @@ import Auth from './pages/Auth';
 import { AuthSigninProvider } from './context/Auth/Signin';
 import EmailVerification from './pages/EmailVerification';
 import { AuthSignupProvider } from './context/Auth/Register';
+import Profile from './pages/Profile';
+import { AuthProfileProvider } from './context/Auth/Profile';
 
 function App() {
   return (
     <BrowserRouter>
       <AuthSignupProvider>
         <AuthSigninProvider>
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path='/auth/login' element={<Auth />} />
-            <Route path='/auth/register' element={<Auth />} />
-            <Route
-              path='/auth/email-verification'
-              element={<EmailVerification />}
-            />
-            <Route path='/auth/profile' element={<ProfileInfo />} />
-            <Route path='/community' element={<Community />} />
-            <Route path='/detail' element={<ChatQADetail />} />
-          </Routes>
+          <AuthProfileProvider>
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path='/auth/login' element={<Auth />} />
+              <Route path='/auth/register' element={<Auth />} />
+              <Route
+                path='/auth/email-verification'
+                element={<EmailVerification />}
+              />
+              <Route path='/auth/profile' element={<Profile />} />
+              <Route path='/community' element={<Community />} />
+              <Route path='/detail' element={<ChatQADetail />} />
+            </Routes>
+          </AuthProfileProvider>
         </AuthSigninProvider>
       </AuthSignupProvider>
     </BrowserRouter>
