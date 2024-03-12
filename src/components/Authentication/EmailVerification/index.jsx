c930f482444a9c9e643a7d8616305703dc72f6c2
Osagie Bello <info@techstackmedia.com>
2024-03-12 18:33:12 +0100

    feat: show alert message when email is sent successfully to inbox

--- src/components/Authentication/EmailVerification/index.jsx
+++ src/components/Authentication/EmailVerification/index.jsx
@@ -4,19 +4,55 @@ import Button from '../../shared/Button';
 import styles from './index.module.css';
 import { useContext } from 'react';
 import { AuthForgotPasswordResetContext } from '../../../context/Auth/ForgotPassword';
+import Alert from '../../shared/Alert';
 
 const EmailVerification = () => {
-  const { email, handleForgotPasswordSubmit, handleEmailChange } = useContext(
-    AuthForgotPasswordResetContext
-  );
+  const {
+    email,
+    handleForgotPasswordSubmit,
+    handleEmailChange,
+    forgotPasswordSuccessMessage,
+    forgotPasswordErrorMessage,
+    forgotPasswordErrorAltMessage,
+  } = useContext(AuthForgotPasswordResetContext);
 
   return (
     <>
+      <div className={styles.alert}>
+        {forgotPasswordSuccessMessage ? (
+          <Alert>
+            <Content cn={`heading ${styles.heading}`}>
+              {forgotPasswordSuccessMessage}
+            </Content>
+            <Content cn={`paragraph ${styles.paragraph}`}>
+              We just sent a message to the email you provided with a link to
+              reset your password. Please check your inbox and follow the
+              instructions in the email.
+            </Content>
+          </Alert>
+        ) : (
+          forgotPasswordErrorMessage ||
+          (forgotPasswordErrorAltMessage && (
+            <Alert>
+              <Content className={`heading ${styles.heading}`}>
+                {forgotPasswordErrorMessage}
+              </Content>
+              <Content className={`paragraph ${styles.paragraph}`}>
+                {forgotPasswordErrorAltMessage}
+              </Content>
+            </Alert>
+          ))
+        )}
+      </div>
       <form
         onSubmit={handleForgotPasswordSubmit}
         className={styles.formEmailVerification}
       >
-        <Content cn={`heading ${styles.heading}`}>Email Verification</Content>
+        <Content cn={`heading ${styles.heading} ${styles.newHeading}`}>
+          {forgotPasswordSuccessMessage
+            ? 'Password Reset Sent'
+            : 'Email Verification'}
+        </Content>
 
         <Input
           type='email'
@@ -26,7 +62,12 @@ const EmailVerification = () => {
           onChange={handleEmailChange}
           placeholder='Enter your email'
         />
-        <Button type='submit'>Verify Your Email</Button>
+        <Button
+          disabled={forgotPasswordSuccessMessage ? true : false}
+          type='submit'
+        >
+          Verify Your Email
+        </Button>
 
         <Content cn={`paragraph ${styles.paragraph}`}>
           Navigate to your email inbox with the subject line{' '}
