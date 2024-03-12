bbee4b35df5aee08cc584c5e30234906e7f1047d
Osagie Bello <info@techstackmedia.com>
2024-03-12 15:55:29 +0100

    feat(css): conditionally navigate to home page or remain in reset-password page

--- src/pages/ResetPassword/index.jsx
+++ src/pages/ResetPassword/index.jsx
@@ -1,11 +1,56 @@
+import { useNavigate } from 'react-router-dom';
 import spinner from '../../images/spinner.gif';
+import Alert from '../../components/shared/Alert';
+import Content from '../../components/shared/Content';
+import styles from './index.module.css';
+import { useContext, useEffect } from 'react';
+import { AuthResetPasswordContext } from '../../context/Auth/ResetPassword';
+
 const ResetPassword = () => {
+  const navigate = useNavigate();
+  const {
+    resetPasswordSuccessMessage,
+    resetPasswordErrorMessage,
+    resetPasswordErrorAltMessage,
+  } = useContext(AuthResetPasswordContext);
   console.log(window.location.href);
-  return (
-    <div className='App'>
-      <img src={spinner} alt='spinner gif' width={150} height={150} />
-    </div>
-  );
+  const token = localStorage.getItem('token');
+
+  useEffect(() => {
+    if (token) {
+      navigate('/');
+    }
+  }, [navigate, token]);
+
+  if (token) {
+    return (
+      <div>
+        {resetPasswordSuccessMessage && (
+          <Alert>{resetPasswordSuccessMessage}</Alert>
+        )}
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <div className={styles.pageAlert}>
+          {resetPasswordErrorMessage && (
+            <Alert>
+              <Content className={`heading ${styles.heading}`}>
+                {resetPasswordErrorMessage}
+              </Content>
+              <Content className={`paragraph ${styles.paragraph}`}>
+                {resetPasswordErrorAltMessage}
+              </Content>
+            </Alert>
+          )}
+        </div>
+        <div className='App'>
+          <img src={spinner} alt='spinner gif' width={150} height={150} />
+        </div>
+      </>
+    );
+  }
 };
 
 export default ResetPassword;
