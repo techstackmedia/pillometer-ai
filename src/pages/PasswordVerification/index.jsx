3d8dedd6ab32270d9b563d811cc4da74ddc1de65
Osagie Bello <info@techstackmedia.com>
2024-03-12 14:17:22 +0100

    create password verification component page by importing navbar and reset password components

---
+++ src/pages/PasswordVerification/index.jsx
@@ -0,0 +1,13 @@
+import ResetPassword from '../../components/Authentication/ResetPassword';
+import Navbar from '../../components/Navbar';
+
+const PasswordVerification = () => {
+  return (
+    <>
+      <Navbar />
+      <ResetPassword />
+    </>
+  );
+};
+
+export default PasswordVerification;
