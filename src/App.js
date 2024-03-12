a31ff68e64d755f7c74f8ce637e2959c0a277a70
Osagie Bello <info@techstackmedia.com>
2024-03-12 14:21:36 +0100

    feat: add routes for reset password, password verification components or pages

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import EmailVerification from './pages/EmailVerification';
 import { AuthSignupProvider } from './context/Auth/Register';
 import Profile from './pages/Profile';
 import { AuthProfileProvider } from './context/Auth/Profile';
+import { AuthForgotPasswordProvider } from './context/Auth/ForgotPassword';
+import PasswordVerification from './pages/PasswordVerification';
+import { AuthResetPasswordProvider } from './context/Auth/ResetPassword';
+import ResetPassword from './pages/ResetPassword';
 
 function App() {
   return (
@@ -16,18 +20,27 @@ function App() {
       <AuthSignupProvider>
         <AuthSigninProvider>
           <AuthProfileProvider>
-            <Routes>
-              <Route index element={<Home />} />
-              <Route path='/auth/login' element={<Auth />} />
-              <Route path='/auth/register' element={<Auth />} />
-              <Route
-                path='/auth/email-verification'
-                element={<EmailVerification />}
-              />
-              <Route path='/auth/profile' element={<Profile />} />
-              <Route path='/community' element={<Community />} />
-              <Route path='/detail' element={<ChatQADetail />} />
-            </Routes>
+            <AuthForgotPasswordProvider>
+              <AuthResetPasswordProvider>
+                <Routes>
+                  <Route index element={<Home />} />
+                  <Route path='/auth/login' element={<Auth />} />
+                  <Route path='/auth/register' element={<Auth />} />
+                  <Route
+                    path='/auth/password-verification'
+                    element={<PasswordVerification />}
+                  />
+                  <Route
+                    path='/auth/email-verification'
+                    element={<EmailVerification />}
+                  />
+                  <Route path='/reset-password' element={<ResetPassword />} />
+                  <Route path='/auth/profile' element={<Profile />} />
+                  <Route path='/community' element={<Community />} />
+                  <Route path='/detail' element={<ChatQADetail />} />
+                </Routes>
+              </AuthResetPasswordProvider>
+            </AuthForgotPasswordProvider>
           </AuthProfileProvider>
         </AuthSigninProvider>
       </AuthSignupProvider>
