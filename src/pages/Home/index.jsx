a1d8d8f8b3a23c97cc702f2191cdd8405dfbfb71
Osagie Bello <info@techstackmedia.com>
2024-03-12 18:49:59 +0100

    chore: remove console logs and unnecerry code

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,21 +6,13 @@ import { useContext, useEffect } from 'react';
 import { AuthSigninContext } from '../../context/Auth/Signin';
 import Alert from '../../components/shared/Alert';
 import styles from './index.module.css';
-import { AuthResetPasswordContext } from '../../context/Auth/ResetPassword';
-import Content from '../../components/shared/Content';
 
 const Home = () => {
   const location = useLocation();
   const token = location.state?.token;
   const status = location.state?.status;
   const profile = location.state?.profile;
-  const details = location.state?.details;
   const { successMessage } = useContext(AuthSigninContext);
-  const {
-    resetPasswordSuccessMessage,
-    resetPasswordErrorMessage,
-    resetPasswordErrorAltMessage,
-  } = useContext(AuthResetPasswordContext);
 
   useEffect(() => {
     if (profile) {
@@ -33,32 +25,15 @@ const Home = () => {
       localStorage.setItem('token', token);
     }
   }, [token]);
-  console.log(resetPasswordSuccessMessage, resetPasswordErrorMessage);
 
   return (
     <>
-      {status && successMessage ? (
+      {status && successMessage && (
         <div className={styles.homeAlert}>
           <Alert>Login Successful</Alert>
         </div>
-      ) : (
-        <div className={styles.homeAlert}>
-          {details ? (
-            <Alert>{details}</Alert>
-          ) : (
-            resetPasswordErrorMessage && (
-              <Alert>
-                <Content className={`heading ${styles.heading}`}>
-                  {resetPasswordErrorMessage}
-                </Content>
-                <Content className={`paragraph ${styles.paragraph}`}>
-                  {resetPasswordErrorAltMessage}
-                </Content>
-              </Alert>
-            )
-          )}
-        </div>
       )}
+
       <Navbar />
       <div className='Main'>
         <Sidebar />
