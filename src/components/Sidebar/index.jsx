e05702231535640418aea1cb3452572d9981eecd
Osagie Bello <info@techstackmedia.com>
2024-03-13 15:04:48 +0100

    feat: create handleNewChat function to create a new post chat request

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,20 +1,39 @@
+/* eslint-disable no-unused-vars */
 import AddIcon from '../../images/add.png';
 import Content from '../shared/Content';
 import styles from './index.module.css';
 import editPenIcon from '../../images/editPen.png';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { NewPostContext } from '../../context/Chat/POST';
+import { useContext, useState } from 'react';
 const Sidebar = () => {
   const navigate = useNavigate();
   const handleCommunityNav = () => {
     navigate('/community');
   };
   const { pathname } = useLocation();
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [responseMessage, setResponseMessage] = useState(null);
+
+  const { createNewPost } = useContext(NewPostContext);
+  const handleNewChat = async () => {
+    const token = localStorage.getItem('token');
+
+    try {
+      const response = await createNewPost(token);
+      setResponseMessage(response);
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
+
+  // console.log(errorMessage, responseMessage);
 
   return (
     <div className={styles.sidebar}>
       <div className={styles.main}>
         <div className={styles.section}>
-          <button className={styles.button}>
+          <button className={styles.button} onClick={handleNewChat}>
             <span>New Chat</span> <img src={AddIcon} alt='add icon' />
           </button>
         </div>
