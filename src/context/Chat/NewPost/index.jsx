8e37675ac6e08ea5d5d64a073fc2e5fc355b1cb3
Osagie Bello <info@techstackmedia.com>
2024-03-13 15:07:19 +0100

    build: create new post context component

---
+++ src/context/Chat/NewPost/index.jsx
@@ -0,0 +1,51 @@
+import { createContext, useState } from 'react';
+import { BASE_CHAT_URL } from '../../../constants';
+
+const NewPostContext = createContext();
+
+const NewPostProvider = ({ children }) => {
+  const [newPostData, setNewPostData] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [errorAltMessage, setAltErrorMessage] = useState(null);
+  const [responseMessage, setResponseMessage] = useState(null);
+
+  const createNewPost = async (token) => {
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `token ${token}`,
+      },
+      body: JSON.stringify({}),
+    };
+
+    try {
+      const response = await fetch(BASE_CHAT_URL, requestOptions);
+      setResponseMessage(response);
+      if (!response.ok) {
+        setAltErrorMessage('Failed to create new post');
+        setTimeout(() => {
+          setAltErrorMessage(null);
+        }, 3000);
+      }
+      const data = await response.json();
+      setNewPostData(data);
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
+
+  const values = {
+    createNewPost,
+    newPostData,
+    errorMessage,
+    errorAltMessage,
+    responseMessage,
+  };
+
+  return (
+    <NewPostContext.Provider value={values}>{children}</NewPostContext.Provider>
+  );
+};
+
+export { NewPostProvider, NewPostContext };
