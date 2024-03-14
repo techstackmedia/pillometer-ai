f9e9a8d122c9fb09901f2dbb8cbc3408740216fc
Osagie Bello <info@techstackmedia.com>
2024-03-14 08:33:17 +0100

    feat: empty state when there's no conversation

--- src/components/Forum/RightCardQA/index.jsx
+++ src/components/Forum/RightCardQA/index.jsx
@@ -4,30 +4,51 @@ import Card from '../../shared/Card';
 import styles from './index.module.css';
 import thumbUpIcon from '../../../images/thumbUp.png';
 import barChatIcon from '../../../images/barChart.png';
+import { useContext } from 'react';
+import { WebSocketContext } from '../../../context/Chat/Service';
+import chatIcon from '../../../images/forum.png';
+import Content from '../../shared/Content';
 
 const RightCardQA = () => {
+  const { response } = useContext(WebSocketContext);
   return (
     <form className={styles.cardForm}>
-      <Card cn={styles.qa}>
-        <div className={styles.viewLikes}>
-          <div className={styles.postView}>Posted 3 days ago</div>
-          <div className={styles.likesSection}>
-            <div className={styles.likes}>
-              <img src={barChatIcon} alt='thumbs up icon' />
-              <span>514</span>
+      <Card cn={`${response.length > 0 ? styles.qa : styles.ht}`}>
+        {response.length > 0 && (
+          <>
+            <div className={styles.viewLikes}>
+              <div className={styles.postView}>Posted 3 days ago</div>
+              <div className={styles.likesSection}>
+                <div className={styles.likes}>
+                  <img src={barChatIcon} alt='thumbs up icon' />
+                  <span>514</span>
+                </div>
+                <div className={styles.likes}>
+                  <span>304</span>
+                  <img src={thumbUpIcon} alt='thumb up icon' />
+                </div>
+              </div>
             </div>
-            <div className={styles.likes}>
-              <span>304</span>
-              <img src={thumbUpIcon} alt='thumb up icon' />
-            </div>
+            <Tag
+              cn={styles.tags}
+              sx={{ justifyContent: 'flex-end', marginBlock: 10 }}
+            />
+            <hr />
+          </>
+        )}
+        {response.length > 0 ? (
+          <ChatResponseList />
+        ) : (
+          <div className={styles.chat}>
+            <img src={chatIcon} alt='chat icon' width={40} height={40} />
+            <Content cn={`heading ${styles.heading}`}>
+              View Conversations
+            </Content>
+            <Content cn={`paragraph ${styles.paragraph}`}>
+              Select a conversation on the left panel to view
+            </Content>
           </div>
-        </div>
-        <Tag
-          cn={styles.tags}
-          sx={{ justifyContent: 'flex-end', marginBlock: 10 }}
-        />
-        <hr />
-        <ChatResponseList />
+        )}
       </Card>
     </form>
   );
