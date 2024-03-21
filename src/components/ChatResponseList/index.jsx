f0457b6d4006b917520bbab5b8cf4350835a5fe9
Osagie Bello <info@techstackmedia.com>
2024-03-21 22:08:03 +0100

    feat: specify users to login when in details page

--- src/components/ChatResponseList/index.jsx
+++ src/components/ChatResponseList/index.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import ChatResponse from '../ChatResponse';
 import Chat from '../Chat';
 import styles from './index.module.css';
@@ -7,12 +7,14 @@ import { ChatDetailContext } from '../../context/ChatDetail';
 import { WebSocketContext } from '../../context/Chat/Service';
 import Content from '../shared/Content';
 import Button from '../shared/Button';
+import { token } from '../../constants';
 
 const ChatResponseList = () => {
   const { chats, chatResponses, refreshKey } = useContext(ChatDetailContext);
   const { handleChatQAResponses } = useContext(ChatDetailContext);
   const { isSent /* , newResponse */ } = useContext(WebSocketContext);
   const { pathname } = useLocation();
+  const navigate = useNavigate();
   const handleClick = () => {
     handleChatQAResponses();
   };
@@ -26,12 +28,20 @@ const ChatResponseList = () => {
   }, [isSent, handleChatQAResponses, refreshKey]);
   // console.log(newResponse);
   // console.log(chatResponses);
+  const navigateToLogin = () => {
+    navigate('/auth/login');
+  };
+
   return (
     <>
       {!chats ? (
         <div className={styles.refresh}>
-          <Content cn='heading'>Start Conversation</Content>
-          <Button navigateToNextPage={handleClick}>Open chat</Button>
+          <Content cn='heading'>
+            {!token ? 'Login to commence conversation' : 'Start Conversation'}
+          </Content>
+          <Button navigateToNextPage={!token ? navigateToLogin : handleClick}>
+            {!token ? 'Login' : 'Open chat'}
+          </Button>
         </div>
       ) : (
         <main className={styles.main}>
