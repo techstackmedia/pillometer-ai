b02c0176cd56fed2cfcb351447db260a8f28f115
Osagie Bello <info@techstackmedia.com>
2024-03-21 21:45:34 +0100

    feat: remove banner if height at home page  is less than 1033

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,13 +1,29 @@
 import Banner from '../shared/Banner';
 import Content from '../shared/Content';
 import styles from './index.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Chat from '../Chat';
 import SymptomList from './SymptomList';
 import SymptomButton from './SymptomButton';
+import { useLocation } from 'react-router-dom';
 
 const Main = () => {
   const [viewMore, setViewMore] = useState(false);
+  const [browserHeight, setBrowserHeight] = useState(window.innerHeight);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const handleResize = () => {
+      setBrowserHeight(window.innerHeight);
+    };
+    console.log(browserHeight);
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [browserHeight]);
 
   const handleViewMoreClick = () => {
     setViewMore((prev) => {
@@ -18,7 +34,9 @@ const Main = () => {
   return (
     <div className={styles.main}>
       <main>
-        <Banner>
+        <Banner
+          cn={pathname === '/' && browserHeight < 1033 && `${styles.dnone}`}
+        >
           <div className={styles.gradientText}>
             <Content cn={`heading ${styles.heading}`}>
               Your Ultimate Wellness Partner
