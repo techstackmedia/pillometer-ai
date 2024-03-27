aea704a8627b160489d72114edb151dd8b4d7f59
Bello Osagie <osagienoah04@gmail.com>
2024-03-27 09:20:59 -0700

    chore: remove the import of react

--- src/components/AlertMessages/index.jsx
+++ src/components/AlertMessages/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useContext } from 'react';
 import { NewPostContext } from '../../context/Chat/NewPost';
 import { ChatDetailContext } from '../../context/ChatDetail';
