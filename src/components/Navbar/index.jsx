6dbd53868936521aa7c98cac1bd54b454e9eb451
Bello Osagie <osagienoah04@gmail.com>
2024-04-29 04:25:18 -0700

    feat: provide menu element to show and hide side bar throughhandleMenuToggle function

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,9 +3,13 @@ import styles from './index.module.css';
 import logo from '../../logo.svg';
 import { useLocation, useNavigate } from 'react-router-dom';
 import ProfileImageInfo from '../ProfileImageInfo';
+import { useContext } from 'react';
+import { NewPostContext } from '../../context/Chat/NewPost';
+import menuIcon from '../../images/menu.png';
 
 const Navbar = () => {
   const { pathname } = useLocation();
+  const { handleMenuToggle } = useContext(NewPostContext);
   const navigate = useNavigate();
   const handleNavigate = () => {
     navigate('/');
@@ -13,10 +17,14 @@ const Navbar = () => {
 
   return (
     <div className={styles.navbar}>
-      <div onClick={handleNavigate}>
+      <div onClick={handleMenuToggle} className={styles.menuButton}>
+        <img src={menuIcon} alt='menu icon' />
+      </div> 
+      <div onClick={handleNavigate} className={styles.logo}>
         <img src={logo} alt='pillometer logo' />
         <Content cn={`heading ${styles.heading}`}>pillometer.ai</Content>
       </div>
+      
       {pathname === '/auth/profile' ? (
         <button type='button' onClick={handleNavigate}>
           Close
