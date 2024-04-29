c47ad92a312856cdce86ee3dbf4bdef5b4fae1da
Bello Osagie <osagienoah04@gmail.com>
2024-04-29 10:59:47 -0700

    style: responsive button component

--- src/components/shared/Button/index.jsx
+++ src/components/shared/Button/index.jsx
@@ -1,8 +1,10 @@
+import { useLocation } from 'react-router-dom';
 import styles from './index.module.css';
 const Button = ({ children, sx, cn, type, disabled, navigatePage }) => {
+  const {pathname} = useLocation()
   return (
     <button
-      className={`${styles.button} ${cn}`}
+      className={`${styles.button} ${!pathname.includes('auth') ? styles.loginButton : ''} ${cn}`}
       style={sx}
       type={type}
       disabled={disabled}
